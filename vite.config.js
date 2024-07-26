080c4af0a374b1906c7b85f94aaed46fcf4f2bf5
chenliwen <liwen.chen@mobvista.com>
2024-07-26 20:38:59 +0800

    index.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import path from 'path';
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+
+import pkg from './package.json' assert {type: 'json'};
 
 const resolve = (url) => path.resolve(__dirname, url);
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@components': resolve('./src/components'),
-      '@assets': resolve('./src/assets'),
-      '@style': resolve('./src/style'),
-      '@stores': resolve('./src/stores/index.js'),
-      '@utils': resolve('./src/utils'),
-      '@hooks': resolve('./src/hooks'),
+    resolve: {
+        alias: {
+            '@components': resolve('./src/components'),
+            '@assets': resolve('./src/assets'),
+            '@style': resolve('./src/style'),
+            '@stores': resolve('./src/stores/index.js'),
+            '@utils': resolve('./src/utils'),
+            '@hooks': resolve('./src/hooks'),
+        },
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'], // 省略扩展名
     },
-    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"], // 省略扩展名
-  },
-  plugins: [react()],
-})
+    plugins: [react()]
+});
